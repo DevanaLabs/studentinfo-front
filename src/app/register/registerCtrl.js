e6fbdcad001cc4debb7c02ba5ffae114ae9b1ea2
Nikola Ninkovic <nikola.ninkovic@labs.devana.rs>
2016-03-09 15:16:55 +0100

    Registration done

---
+++ src/app/register/registerCtrl.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('siApp')
+  .controller('RegisterCtrl', ['$scope', '$stateParams', '$state', 'Error', 'Register',
+    function ($scope, $stateParams, $state, Error, Register) {
+      $scope.credentials = {
+        username: '',
+        password: {
+          password: '',
+          passwordConfirmation: ''
+        }
+      };
+
+      Register.getUser($stateParams.registerToken).then(function (response) {
+        if (response.data.success) {
+          $scope.credentials.username = response.data.success.data.user.email.email;
+        }
+      }, function (response) {
+        Error.httpError(response);
+      });
+
+      $scope.onSubmit = function () {
+        if (Register.validatePassword($scope.credentials.password, $scope.credentials.passwordConfirmation)) {
+          Register.registerUser($stateParams.registerToken, $scope.credentials.password).then(function (response) {
+            if (response.data.success) {
+              Error.success('REGISTER.SUCCESS');
+              $state.go('login');
+            }
+          }, function (response) {
+            Error.error(response);
+          });
+        } else {
+          Error.error('INVALID_INPUT_DATA');
+        }
+      };
+    }]);
